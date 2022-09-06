b7e5db5589b24dd1fbc23d2c978886bff32e2748
Weisman <weisman123@outlook.com>
2022-09-06 08:31:55 -0700

    dms my main account messages its dmed

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client();
-require('dotenv').load();
+require('dotenv').config();
 
 client.on('ready', async () => {
     console.log(`${client.user.username} is ready!`);
 })
 
-client.login(proces.env.ALT_TOKEN);
+client.on('messageCreate', async (message) => {
+    const mainAccount = await client.users.fetch(process.env.MAIN_ID)
+
+    if ((message.channel.type === 'DM') && (message.author.id !==process.env.ALT_ID) && (message.author.id !== process.env.MAIN_ID)) {
+        mainAccount.send(message.content)
+    }
+})
+
+client.login(process.env.ALT_TOKEN);
